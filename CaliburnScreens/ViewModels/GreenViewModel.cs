f5fb5704c552fd28a5ca3395e53273380d274613
Jacek Korpusik <j.korpusik@wp.mil.pl>
2014-07-22 14:20:16 +0200

    Dodanie data validation

--- CaliburnScreens/ViewModels/GreenViewModel.cs
+++ CaliburnScreens/ViewModels/GreenViewModel.cs
@@ -1,31 +1,45 @@
 ﻿using Caliburn.Micro;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro.Validation;
+using System.ComponentModel;
+
+
 
 namespace CaliburnScreens.ViewModels
 {
-    public class GreenViewModel :Screen
+    public class GreenViewModel :ValidatingScreen<GreenViewModel>
     {
         
 
         private string _destinationField = "";
+        
         public string DestinationField 
         {
             get { return _destinationField;}
+            
+            
             set
             {
+                
                 _destinationField = value;
                 NotifyOfPropertyChange(() => DestinationField);
             }
         }
 
         private string _searchField = "";
+
+        [Required(ErrorMessage = "SearchField is required.")]
+        [MinLength(3,ErrorMessage="Tekst powinien być dłuższy niż 3 znaki.")]
         public string SearchField
         {
             get { return _searchField;}
+            
             set
             {
                 
@@ -46,6 +60,7 @@ namespace CaliburnScreens.ViewModels
 
         public void ForceCopyField()
         {
+            
             DestinationField = SearchField;
         }
 
@@ -62,7 +77,29 @@ namespace CaliburnScreens.ViewModels
 
 
         }
+        /*
+                public string Error
+                {
+                    get { return null; }
+                }
 
+                public string this[string columnName]
+                {
+                    get
+                    {
+                        switch (columnName)
+                        {
+                            case "SearchField":
+                                if (String.IsNullOrEmpty(this.SearchField) || this.SearchField.Length < 3)
+                                    return "Wpis nie może być krótszy niż 3 znaki!";
+                                break;
+                        }
+
+                        return string.Empty;
+                    }
+                }
+
+                */
         
     }
 }
